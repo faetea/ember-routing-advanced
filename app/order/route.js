5321ed534a68cc4da14ebd7bef3dc36fc913353f
saphron <saphron.scul@gmail.com>
2015-12-08 14:42:32 -0500

    edited order(s) route

--- app/order/route.js
+++ app/order/route.js
@@ -4,20 +4,23 @@ export default Ember.Route.extend({
   model: function(params){
     return [
       {
+        id: 1,
         orderDate: '2015-10-16',
         totalCost: 270,
         purchaser: "Carmen's Kitchen"
       },
       {
+        id: 2,
         orderDate: '2015-11-30',
         totalCost: 540,
         purchaser: "Lovin' Oven"
       },
       {
+        id: 3,
         orderDate: '2015-12-08',
         totalCost: 135,
         purchaser: "Amelia Brent"
       },
-    ][params.product_id - 1]; // not real life
+    ][params.order_id - 1]; // not real life
   }
 });
--- app/orders/route.js
+++ app/orders/route.js
@@ -4,16 +4,19 @@ export default Ember.Route.extend({
   model: function(){
     return [
       {
+        id: 1,
         orderDate: '2015-10-16',
         totalCost: 270,
         purchaser: "Carmen's Kitchen"
       },
       {
+        id: 2,
         orderDate: '2015-11-30',
         totalCost: 540,
         purchaser: "Lovin' Oven"
       },
       {
+        id: 3,
         orderDate: '2015-12-08',
         totalCost: 135,
         purchaser: "Amelia Brent"
